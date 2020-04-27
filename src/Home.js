12ca8484ecdf334bf95ed5bea57744415ef3cc34
Abdullahi <abdullahihassanke@gmail.com>
2020-04-27 19:38:18 +0300

    styled components

--- src/Home.js
+++ src/Home.js
@@ -20,10 +20,16 @@ import { useHomeFetch } from "./components/hooks/useHomeFetch";
 const Home = () => {
   const [{ state, loading, error }, fetchMovies] = useHomeFetch();
   console.log(state);
+  if (error) return <div>Something went wrong</div>;
+  if (!state.movies[0]) return <Spinner />;
   return (
     <React.Fragment>
       <Header />
-      <Hero />
+      <Hero
+        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
+        title={state.heroImage.original_title}
+        text={state.heroImage.overview}
+      />
       <SearchBar />
       <Grid />
       <Thumbnail />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,41 @@
 import React from "react";
+import styled from "styled-components";
+import image from "../images/lmdb.png";
 
-const Header = () => <div>Header</div>;
+const StyledHeader = styled.div`
+  background: #1c1c1c;
+  padding: 0 20px;
+  box-sizing: border-box;
+
+  .header-content {
+    max-width: 1280px;
+    height: 20vh;
+    padding: 20px 0;
+    margin: 0 auto;
+    box-sizing: border-box;
+
+    @media screen and (max-width: 500px) {
+      min-height: 0px;
+    }
+  }
+`;
+
+const StyledLogo = styled.img`
+  width: 250px;
+  margin-top: 0px;
+
+  @media screen and (max-width: 500px) {
+    width: 150px;
+    margin-top: 5px;
+  }
+`;
+
+const Header = () => (
+  <StyledHeader>
+    <div className="header-content">
+      <StyledLogo src={image} alt="lmdb" />
+    </div>
+  </StyledHeader>
+);
 
 export default Header;
